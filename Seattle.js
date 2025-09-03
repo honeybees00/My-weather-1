046c16c0e24db8bb5508b444ea404a49f273593c
honeybees00 <Cortizmason@outlook.com>
2025-09-03 14:01:22 -0400

    1st debug

---
+++ Seattle.js
@@ -0,0 +1,31 @@
+{
+    "latitude": 47.0,
+    "longitude": 122.0,
+    "generationtime_ms": 0.049233436584472656,
+    "utc_offset_seconds": 0,
+    "timezone": "GMT",
+    "timezone_abbreviation": "GMT",
+    "elevation": 437.0,
+    "current_weather_units": {
+        "time": "iso8601",
+        "interval": "seconds",
+        "temperature": "°C",
+        "windspeed": "km/h",
+        "winddirection": "°",
+        "is_day": "",
+        "weathercode": "wmo code"
+    },
+    "current_weather": {
+        "time": "2025-09-02T16:45",
+        "interval": 900,
+        "temperature": 15.6,
+        "windspeed": 4.8,
+        "winddirection": 13,
+        "is_day": 0,
+        "weathercode": 3
+    }
+}
+// Display Atlanta weather data
+setText("temp", atlantaWeather.current_weather.temperature);
+setText("wind", atlantaWeather.current_weather.windspeed);
+setText("code", atlantaWeather.current_weather.weathercode);
